4a48bc23604c0e989d6d03ad7921a1936ae6465e
Ashutosh741 <aashutoshmishra296@gmail.com>
2023-07-25 01:06:19 +0530

    home page work done

--- script.js
+++ script.js
@@ -41,14 +41,64 @@
 
 console.log("hello connected")
 
+
+// $('.slider').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// });
+	
+
+
 $('.slider').slick({
   dots: false,
   infinite: true,
-  speed: 30,
+  speed: 1000,
   autoplay: true,
   autoplaySpeed: 2000,
   slidesToShow: 4,
-  slidesToScroll: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+$('.processSlider').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
