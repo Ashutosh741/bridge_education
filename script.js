f53a0d07e7f53bb1ae824f1e16537a83e2e36384
Ashutosh741 <aashutoshmishra296@gmail.com>
2023-07-27 11:48:57 +0530

    playImage

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 console.log("hello connected")
 
 
+
+$(document).ready(function(){
+  $('.navbar-toggler').click(function() {
+    $('.navbar').toggleClass('bg-yellow');
+    $('.navbar-brand img').toggleClass('d-none');
+    $('.nav-link').toggleClass('fw-bold');
+    
+});
+});
+
+
+$(document).ready(function() {
+  // Get the current URL path and extract the page name
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+
+  // Remove the extension from the page name if it has one
+  page = page.replace(".html", "");
+
+  // Add the "active" class to the corresponding nav-item link
+  $("#" + page + "-link").addClass("active");
+}); 
+
+
 // $('.slider').slick({
 //   slidesToShow: 3,
 //   slidesToScroll: 1,
